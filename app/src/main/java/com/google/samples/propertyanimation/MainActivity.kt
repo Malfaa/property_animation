360b893914d9c3b768f4c9ea07d88225e51d8551
gustavo <7110999@gmail.com>
2022-06-14 17:23:17 -0300

    Source Code

---
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.propertyanimation
+
+import android.animation.*
+import android.annotation.SuppressLint
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var star: ImageView
+    private lateinit var rotateButton: Button
+    private lateinit var translateButton: Button
+    private lateinit var scaleButton: Button
+    private lateinit var fadeButton: Button
+    private lateinit var colorizeButton: Button
+    private lateinit var showerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        star = findViewById(R.id.star)
+        rotateButton = findViewById(R.id.rotateButton)
+        translateButton = findViewById(R.id.translateButton)
+        scaleButton = findViewById(R.id.scaleButton)
+        fadeButton = findViewById(R.id.fadeButton)
+        colorizeButton = findViewById(R.id.colorizeButton)
+        showerButton = findViewById(R.id.showerButton)
+
+        rotateButton.setOnClickListener {
+            rotater()
+        }
+
+        translateButton.setOnClickListener {
+            translater()
+        }
+
+        scaleButton.setOnClickListener {
+            scaler()
+        }
+
+        fadeButton.setOnClickListener {
+            fader()
+        }
+
+        colorizeButton.setOnClickListener {
+            colorizer()
+        }
+
+        showerButton.setOnClickListener {
+            shower()
+        }
+    }
+
+     private fun ObjectAnimator.disableViewRotation(view: View){
+         addListener(object : AnimatorListenerAdapter() {
+             override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
+                 view.isEnabled = false
+             }
+
+             override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                 view.isEnabled = true
+             }
+         })
+     }
+
+    private fun rotater() {
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+        animator.disableViewRotation(rotateButton)
+        animator.start()
+    }
+
+    private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewRotation(translateButton)
+        animator.start()
+    }
+
+    private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewRotation(scaleButton)
+        animator.start()
+
+    }
+
+    private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.repeatCount = 1
+        animator.disableViewRotation(fadeButton)
+        animator.start()
+    }
+
+    @SuppressLint("ObjectAnimatorBinding")
+    private fun colorizer() {
+        val animator = ObjectAnimator.ofArgb(star.parent, "backgroundColor", Color.BLACK, Color.RED)
+        animator.duration = 500
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewRotation(colorizeButton)
+
+        animator.start()
+    }
+
+    private fun shower() {
+        val container = star.parent as ViewGroup
+        val containerH = container.height
+        val containerW = container.width
+        var starH = star.height.toFloat()
+        var starW = star.width.toFloat()
+
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT)
+
+        container.addView(newStar)
+
+        newStar.scaleX = Math.random().toFloat() * 1.5f + .1f
+        newStar.scaleY = newStar.scaleX
+        starW *= newStar.scaleX
+        starH *= newStar.scaleY
+
+        //Spawn
+        newStar.translationX = Math.random().toFloat() * containerW - starW/2
+
+        //Trajeto
+        val mover = ObjectAnimator.ofFloat(newStar, View.TRANSLATION_Y, -starH, containerH + starH)
+        mover.interpolator = AccelerateInterpolator(1f)
+
+        //Rotação
+        val rotator = ObjectAnimator.ofFloat(newStar, View.ROTATION, (Math.random() * 1080).toFloat())
+        rotator.interpolator = LinearInterpolator()
+
+        val set = AnimatorSet()
+        set.playTogether(mover, rotator)
+        set.duration = (Math.random() + 1500 + 500).toLong()
+
+        //Remove a view que saiu da tela
+        set.addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                container.removeView(newStar)
+            }
+        })
+
+        set.start()
+    }
+
+}
